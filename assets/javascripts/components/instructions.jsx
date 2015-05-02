1b618a4eb02a62507d0d749613cb08b8ac7fd7bc
Daniel Howard <dhoward@tongal.com>
2015-05-02 11:22:07 -0700

    Updated instructions

--- assets/javascripts/components/instructions.jsx
+++ assets/javascripts/components/instructions.jsx
@@ -22,17 +22,19 @@ var Instructions = React.createClass({
                 <span aria-hidden="true">&times;</span>
                 <span className="sr-only">Close</span>
               </button>
-              <h3 className="modal-title">Welcome to the Draft Party!</h3>
+              <h3 className="modal-title">Welcome to the <span className="logo">Draft Party!</span></h3>
             </div>
             <div className="modal-body">
-              <h4 className="highlighted">First, create an account to customize your rankings</h4>
-              <p>Or, you know, don't.</p>
+
+              <h4 className="highlighted">Drag and drop players to customize your rankings</h4>
+              <p>Or double-click a ranking to enter a new one manually. Either way, they&apos;ll be saved if you&apos;re logged in.</p>
+
               <h4 className="highlighted">During the draft, click on a player to mark him as taken</h4>
-              <p>Or double-click him to say that he's on your team.  He'll then show up in your team on the right hand side.</p>
-              <h4 className="highlighted">Drag and drop the players on your team to rearrange your lineup</h4>
-              <p>Or just leave it alone and your team will autofill based on position</p>
-              <h4 className="highlighted">The stuff in the toolbar is self-explanatory</h4>
-              <p>Except maybe the pencil icons. You can use them to highlight players in the list, if you so choose.</p>
+              <p>Or double-click him to mark him as yours. He&apos;ll show up in your team on the right hand side.</p>
+
+              <h4 className="highlighted">Use the pencil icons to highlight players in yellow or red</h4>
+              <p>If you&apos;re still not happy there are more settings in the main menu.</p>
+
             </div>
             <div className="modal-footer">
               <button className="btn btn-default" type="button" data-dismiss="modal">Close</button>
